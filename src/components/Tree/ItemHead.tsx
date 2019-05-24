15a51f2d6c7b9991ee7fc0590cb2461d40a3fd53
MetromDouble <metromdouble@gmail.com>
2019-05-24 03:36:32 +0300

    Tree expanding (fixes)

--- src/components/Tree/ItemHead.tsx
+++ src/components/Tree/ItemHead.tsx
@@ -5,8 +5,8 @@ interface IItemHead {
 }
 export const ItemHead = styled.div<IItemHead>`
   height: 24px;
-  margin-left: -12px;
-  padding-left: 4px;
+  margin-left: -10px;
+  padding-left: 2px;
   padding-right: 4px;
   background: ${props => props.active ? props.theme.palette.iron : 'transparent'};
   transition: background 0.1s ease;
